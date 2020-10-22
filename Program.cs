ab8771cc724884a92ea60186a508037f84dc99ec
Luis <m.luishernandez@outlook.com>
2020-10-22 11:55:22 -0400

    chnaged the while loop to a for loop

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace Multiple_Greetings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("How many greetings you want?");
+            int greetings = Int32.Parse(Console.ReadLine());
+
+            for (int stop=0; greetings>stop; greetings--)
+            {
+                Console.WriteLine("Hola");
+            }
+
+            
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Multiple_Greetings
 Allow the user to enter an integer that represents a number of times a message will display
+
